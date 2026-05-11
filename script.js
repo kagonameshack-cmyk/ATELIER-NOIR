c2d710db451be48c626567591312e578db110c2e
kagonameshack-cmyk <kagonameshack@gmail.com>
2026-05-11 17:58:04 +0300

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,55 @@
+/* NAVBAR BACKGROUND ON SCROLL */
 
+const navbar = document.querySelector("nav");
+
+window.addEventListener("scroll", () => {
+
+    if(window.scrollY > 50){
+
+        navbar.classList.add("scrolled");
+
+    } else {
+
+        navbar.classList.remove("scrolled");
+
+    }
+
+});
+
+/* SCROLL REVEAL ANIMATION */
+
+const revealElements =
+document.querySelectorAll(
+".philosophy, .project-card, .parallax, .process-item, .contact"
+);
+
+const revealOnScroll = () => {
+
+    revealElements.forEach(element => {
+
+        const windowHeight =
+        window.innerHeight;
+
+        const revealTop =
+        element.getBoundingClientRect().top;
+
+        const revealPoint = 120;
+
+        if(revealTop < windowHeight - revealPoint){
+
+            element.classList.add("active");
+
+        }
+
+    });
+
+};
+
+window.addEventListener(
+"scroll",
+revealOnScroll
+);
+
+/* INITIAL CHECK */
+
+revealOnScroll();
